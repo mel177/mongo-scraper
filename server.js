e950e009d8aac2ec043ac6332772a5bce427de62
Melinh Mai <melinh.mai@gmail.com>
2018-09-07 13:40:12 -0500

    models

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = 3030;
 
 // Initialize Express
 var app = express();
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/week18Populater");
+mongoose.connect("mongodb://localhost/mongo-scraper");
 
 // Routes
 
